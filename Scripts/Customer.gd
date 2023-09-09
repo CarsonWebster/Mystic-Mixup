077a1232ac676284023e1f557eec25976b981fd8
CarsonWebster <CarsonWebster@hotmail.com>
2023-09-09 15:10:01 -0700

    Customer animation

--- Scripts/Customer.gd
+++ Scripts/Customer.gd
@@ -13,7 +13,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("customerWalk")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/Tavern.gd
+++ Scripts/Tavern.gd
@@ -14,11 +14,17 @@ extends Node2D
 # 
 #################################
 
+# Export the customer scene as a PackedScene variable
+@export var customer_scene : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Spawn Customer
+	var customer = customer_scene.instantiate()
+	customer.position = Vector2(1093, 45)
+	add_child(customer)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
