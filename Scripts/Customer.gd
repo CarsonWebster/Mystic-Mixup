d3ce3b20424e601d6f7779de1f1179dfc56cba29
CarsonWebster <CarsonWebster@hotmail.com>
2023-09-09 14:30:28 -0700

    Door and placeholder scripts with todos

---
+++ Scripts/Customer.gd
@@ -0,0 +1,21 @@
+extends Node
+
+#################################
+# RESPONSIBILITIES
+#
+# 1. Set sprite                    [ ]
+# 2. Set sounds                    [ ]
+# 3. Listen for product signals    [ ]
+# 4. Act accordingly               [ ]
+# 5. Signal action to game manager [ ]
+# 
+#################################
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Scripts/Tavern.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+#################################
+# RESPONSIBILITIES
+#
+# 1. Manage ingredents    [ ]
+# 2. Manage customers     [ ]
+# 3. Manage score         [ ]
+# 4. Manage timers        [ ]
+# 5. Manage game over     [ ]
+# 6. Manage craft button  [ ]
+# 7. Manage UI menus      [ ]
+# 
+#################################
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
