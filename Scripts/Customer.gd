cadcfed5db672c1a1a70840147af81007b180588
CarsonWebster <CarsonWebster@hotmail.com>
2023-09-09 15:50:44 -0700

    Dialog scroll animation

--- Scripts/Customer.gd
+++ Scripts/Customer.gd
@@ -14,8 +14,16 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("customerWalk")
+	#$AnimationPlayer.animation_finished
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	#print(anim_name)
+	if anim_name == "customerWalk":
+		$ScrollDialouge/ScrollText.text = "I WANNA ROCK ROCK!!! DUN DUN DUN DUN DUN DUN DUN I WANT TO ROCK ROCK"
+		$AnimationPlayer.play("Dialog")
